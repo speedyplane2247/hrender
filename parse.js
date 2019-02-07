3d4ee6a15ec15a0e5a8305cfb65d54ad1a0a7ea6
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-07 12:48:30 -0500

    1.0-4-alpha

--- parse.js
+++ parse.js
@@ -5,14 +5,51 @@ setTimeout(function() {
     if (render.state == "game" && render.activeState == 1) {
         render.maxFPS_timer = 1000 / render.maxFPS 
         
-    } // dont translate FPS_timer when window is inactive
+     // dont translate FPS_timer when window is inactive
     if (tabActive == false) {
         render.activeState = 0
         render.maxFPS_timer = (1000 / render.maxFPS_menu) + render.engineSleeponIdle // limits fps
     }
     if (tabActive == true) {
         render.activeState = 0
-        render.maxFPS_timer = (1000 / render.maxFPS_menu) + render.engineSleeponActive // limits fps
+        render.maxFPS_timer = (1000 / render.maxFPS_menu) + render.engineSleeponActive
+    } // limits fps
+    if (cl.showfps == 1 || cl.showfps == 2) {
+        if (cl.showfps == 1) {
+        showFPS() // calls renderer to show fps
+        } else {
+            // do nothing
+        }
+    } else {
+        // if non-1/2
+        notshowFPS()
+    }
+    if (cl.showmap == 1 || cl.showmap == 2) {
+        if (cl.showmap == 1) {
+        showMAP()
+        } else {
+            // do nothing
+        }
+    } else {
+        notshowMAP()
+        // restarts client
+    }
+    if (cl.textenable != 1) {
+        // do nothing yet, will disable input once input is enabled
+    }
+    if (cl.restartgame != 0) {
+        location.reload()
+    }
+    if (engine.type == "server" && sv.cheats == 1) {
+        server.push("sv.cheats 1") // sv.cheats to clients
+    }
+    if (engine.type == "server-m" ) {
+        serverM.push(player) // push [player].* to clients
+    }
+    if (sv.cheats == "1") {
+        engine.misctoken[0] = "auth"
+    }
+    
     } else {
         render.maxFPS_timer = 1000 / render.maxFPSmenu
         render.state = "game" // not optimal but quick fix
--- render.js
+++ render.js
@@ -2,6 +2,24 @@
 function showFPS() {
     // not implemented
 }
-
+function notshowFPS() { 
+    // requires engine restart
+}
+function notshowMAP() {
+    // requires engine restart
+}
+function drawFrame(map, player, health, options) {
+    // hmm.exe
+}
 // Used Code
 
+
+/* EXAMPLE RENDER | SOF
+000 FPS | Map: xx_xxxxxx | engine.console
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+X New Game                                X
+X Load Game                               X
+X Options                                 X
+X Quit                                    X
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+*/ // EXAMPLE RENDER | EOF
